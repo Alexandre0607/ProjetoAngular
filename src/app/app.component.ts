d1578a2cf49b01b98fa674f892c7465a674b68be
Xandão <125466131+Alexandre0607@users.noreply.github.com>
2024-09-29 21:54:44 +0000

    musica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, FormsModule], //CommonModule -> para funcionar o *ngFor
 })
-export class AppComponent {
-  constructor() {}
+export class HomePage {
+  public nome = "";
+
+  public items: any[] = [ //any -> os itens podem ser qualquer tipo
+    {
+      titulo: "Forró"
+    },
+    {
+      titulo: "Rock"
+    },
+    {
+      titulo: "Sertanejo"
+    },
+    {
+      titulo: "Funk"
+    }
+  ]
+
+  constructor() { }
+
+  adicionar() {
+    //Adicionando no array
+    this.items.push(
+      {
+        titulo: this.nome
+      }
+    );
+
+    //Limpando campo nome
+    this.nome = "";
+  }
+
+  remover(posicao:number) {
+    this.items.splice(posicao, 1);
+  }
 }
